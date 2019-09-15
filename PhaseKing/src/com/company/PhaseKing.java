3c04c244bac4c4f7aa19385a04c48accb835275f
fernandolevy <fernandoassis@alunos.utfpr.edu.br>
2019-09-15 19:14:21 -0300

    falta phaseking

---
+++ PhaseKing/src/com/company/PhaseKing.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class PhaseKing {
+    public static void main(String args[]) {
+        boolean v = Boolean.parseBoolean(args[0]);
+        int f = Integer.valueOf(args[1]);
+        for (int phase = 1; phase<= f+1; phase++){
+
+        }
+    }
+
+}
---
+++ PhaseKing/src/com/company/process.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class process extends MulticastPeer{
+    public static void main(String args[]) {
+        MulticastPeer.main(new String[] {"ola", "228.5.6.7"});
+    }
+}
