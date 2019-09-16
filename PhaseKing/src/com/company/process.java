87f26865ef7756075241fc1c227ab6976dc7e14f
WilianCavassin <wilianhenri@hotmail.com>
2019-09-15 23:41:02 -0300

    configuração de rand e id de processo 0

--- PhaseKing/src/com/company/process.java
+++ PhaseKing/src/com/company/process.java
@@ -4,5 +4,6 @@ public class process extends MulticastPeer{
     static int rand = new Random().nextInt(1);
     public static void main(String args[]) {
         MulticastPeer.main(new String[] {Integer.toString(rand), "228.5.6.7","0"});
+        //acima, a sequencia de envio é v (valor randomico booleano), endereço de multicast, id de processo neste caso 0.
     }
 }
