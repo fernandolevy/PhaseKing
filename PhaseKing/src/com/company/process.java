6747ce07da05efd956c0080cf5e0119879892616
fernandolevy <fernandoassis@alunos.utfpr.edu.br>
2019-09-15 21:21:35 -0500

    round 1

--- PhaseKing/src/com/company/process.java
+++ PhaseKing/src/com/company/process.java
@@ -1,7 +1,13 @@
 package com.company;
+import java.util.Random;
 
 public class process extends MulticastPeer{
     public static void main(String args[]) {
-        MulticastPeer.main(new String[] {"ola", "228.5.6.7"});
+        MulticastPeer.main(new String[] {"1", "228.5.6.7", "0"});
+        MulticastPeer.main(new String[] {"2", "228.5.6.7", "1"});
+        MulticastPeer.main(new String[] {"3", "228.5.6.7", "1"});
+        MulticastPeer.main(new String[] {"4", "228.5.6.7", "0"});
+        MulticastPeer.main(new String[] {"5", "228.5.6.7", "1"});
+
     }
 }
