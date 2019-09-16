9031651cf2ce3c1522f3aa48c37c176e97b08f94
WilianCavassin <wilianhenri@hotmail.com>
2019-09-15 23:37:33 -0300

    configuração de rand e id de processo 0

--- PhaseKing/src/com/company/process.java
+++ PhaseKing/src/com/company/process.java
@@ -1,8 +1,8 @@
 package com.company;
-import java.util.Random;
+        import java.util.Random;
 public class process extends MulticastPeer{
     static int rand = new Random().nextInt(1);
     public static void main(String args[]) {
-        MulticastPeer.main(new String[] {Integer.toString(rand), "228.5.6.7"});
+        MulticastPeer.main(new String[] {Integer.toString(rand), "228.5.6.7","0"});
     }
 }
