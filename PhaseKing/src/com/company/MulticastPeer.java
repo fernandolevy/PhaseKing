989404d8b117d440adc3162a846c192219480d71
WilianCavassin <wilianhenri@hotmail.com>
2019-09-16 20:29:09 -0300

    multicast versão 2

--- PhaseKing/src/com/company/MulticastPeer.java
+++ PhaseKing/src/com/company/MulticastPeer.java
@@ -2,28 +2,45 @@ package com.company;
 
 import java.net.*;
 import java.io.*;
-public class MulticastPeer{
-    public static void main(String args[]){
-        // args give message contents and destination multicast group (e.g. "228.5.6.7")
-        MulticastSocket s;
-        s = null;
+import java.util.Arrays;
+
+public class MulticastPeer {
+    public static void main(String args[]) throws IOException {
+        //inicializa socket como nulo
+        MulticastSocket s = null;
+        //inicializa o vetor de processos e valores recebidos e controle 1
+        String[] processos = {""};
+        String[] valores = {""};
+        //variaveis de controle (phase e round)
+        int phase = 0;
+        int round = 0;
+        int p_ini = 0;
+        int i_ini = 0;
+        //verificar existencia de processos na rede, esperar ack de todos
         try {
-            InetAddress group = InetAddress.getByName(args[1]);
             s = new MulticastSocket(6789);
+            InetAddress group = InetAddress.getByName(args[1]);
             s.joinGroup(group);
-            byte [] m = args[0].getBytes();
-            DatagramPacket messageOut = new DatagramPacket(m, m.length, group, 6789);
-            s.send(messageOut);
+            byte[] data = args[0].getBytes();
+            DatagramPacket alfa = new DatagramPacket(data, data.length, group, 6789);
+            s.send(alfa);
             byte[] buffer = new byte[1000];
-            for(int i=0; i< 3;i++) {		// get messages from others in group
-                DatagramPacket messageIn = new DatagramPacket(buffer, buffer.length);
-                s.receive(messageIn);
-                System.out.println("Received:" + new String(messageIn.getData()));
+            while (processos.length < 5) {
+                DatagramPacket beta = new DatagramPacket(buffer, buffer.length);
+                s.receive(beta);
+                processos[i_ini] = Integer.toString(p_ini);
+                valores[i_ini] = new String(beta.getData());
+                p_ini++;
+                i_ini++;
+                System.out.println("Received:" + new String(beta.getData()));
             }
-            s.leaveGroup(group);
-        }catch (SocketException e){System.out.println("Socket: " + e.getMessage());
-        }catch (IOException e){System.out.println("IO: " + e.getMessage());
-        }finally {if(s != null) s.close();}
+        } catch (SocketException e) {
+            System.out.println("Socket: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IO: " + e.getMessage());
+        } finally {
+            if (s != null) s.close();
+        }
     }
 
 }
--- PhaseKing/src/com/company/process.java
+++ PhaseKing/src/com/company/process.java
@@ -1,9 +1,10 @@
 package com.company;
+        import java.io.IOException;
         import java.util.Random;
 public class process extends MulticastPeer{
     static int rand = new Random().nextInt(1);
-    public static void main(String args[]) {
-        MulticastPeer.main(new String[] {Integer.toString(rand), "228.5.6.7","0"});
+    public static void main(String args[]) throws IOException {
+        MulticastPeer.main(new String[] {Integer.toString(rand), "228.5.6.7"});
         //acima, a sequencia de envio é v (valor randomico booleano), endereço de multicast, id de processo neste caso 0.
     }
 }
