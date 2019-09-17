cbbc7feca433e4295fb0d6d7dc10d2bfa0a7ec26
fernandolevy <fernandoassis@alunos.utfpr.edu.br>
2019-09-16 19:54:56 -0500

    phaseking

--- PhaseKing/src/com/company/PhaseKing.java
+++ PhaseKing/src/com/company/PhaseKing.java
@@ -8,12 +8,14 @@ import java.net.SocketException;
 
 public class PhaseKing {
     public static void main(String args[]) {
-        String i = args[0];
-        String v = args[3];
+        int i = Integer.valueOf(args[2]);
+        String v = args[0];
+        String tiebreaker = null;
         int one = 0;
-        int majority;
+        int majority = 0;
         int mult = 0;
-        int two = 0;
+        int zero = 0;
+        int n = 5;
         int f = Integer.valueOf(args[1]);
         for (int phase = 1; phase<= f+1; phase++){
             // args give message contents and destination multicast group (e.g. "228.5.6.7")
@@ -26,38 +28,51 @@ public class PhaseKing {
                 DatagramPacket messageOut = new DatagramPacket(m, m.length, group, 6789);
                 s.send(messageOut);
                 byte[] buffer = new byte[1000];
-                for(int j=0; j< 5;j++) {		// get messages from others in group
+                for(int j=0; j< n;j++) {		// get messages from others in group
                     DatagramPacket messageIn = new DatagramPacket(buffer, buffer.length);
                     s.receive(messageIn);
                     System.out.println("Received:" + new String(messageIn.getData()));
-                    if (Integer.valueOf(String.valueOf(messageIn.getData())) == 1) {
+                    if (String.valueOf(messageIn.getData()) == "1") {
                         one = one + 1;
                     }
                     else {
-                        two = two + 1;
+                        zero = zero + 1;
                     }
                 }
-                if (one > 5/2) {
+                if (one > n/2) {
                     majority = 1;
                     mult = one;
                 }
-                else if (two > 5/2) {
-                    majority = 2;
-                    mult = two;
+                else if (zero > n/2) {
+                    majority = 0;
+                    mult = zero;
+                }
+                if (i == phase) {
+                    m = String.valueOf(majority).getBytes();
+                    messageOut = new DatagramPacket(m, m.length, group, 6789);
+                    s.send(messageOut);
+                    buffer = new byte[1000];
+                    for(int j=0; j< 5;j++) {		// get messages from others in group
+                        DatagramPacket messageIn = new DatagramPacket(buffer, buffer.length);
+                        s.receive(messageIn);
+                        System.out.println("Received:" + new String(messageIn.getData()));
+                    }
+                    tiebreaker = v;
+                    if (mult > n/2 + f) {
+                        v = String.valueOf(majority);
+                    }else{
+                        v = tiebreaker;
+                    }
+                    if (phase == f + 1){
+                        System.out.println("output decision value v:" + v);
+                    }
                 }
                 s.leaveGroup(group);
             }catch (SocketException e){System.out.println("Socket: " + e.getMessage());
             }catch (IOException e){System.out.println("IO: " + e.getMessage());
             }finally {if(s != null) s.close();}
-//                (1g) Execute the following Round 2 actions: // actions in round two of each phase
-//                (1h) if i = phase then // only the phase leader executes this send step
-//                        (1i) broadcast majority to all processes;
-//                (1j) receive tiebreaker from Pphase (default value if nothing is received);
-//                (1k) if mult > n/2 + f then
-//                        (1l) v ←− majority;
-//                (1m) else v ←− tiebreaker;
-//                (1n) if phase = f + 1 then
-//                        (1o) output decision value v.
+
+
         }
     }
 
