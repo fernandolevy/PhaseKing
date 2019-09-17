76b7572acdbe1831976a8e6bc883b2def04a0f93
WilianCavassin <wilianhenri@hotmail.com>
2019-09-17 01:34:56 -0300

    comentários

--- PhaseKing/src/com/company/MulticastPeer.java
+++ PhaseKing/src/com/company/MulticastPeer.java
@@ -13,38 +13,49 @@ public class MulticastPeer {
         ArrayList<String> processos = new ArrayList<String>();
         ArrayList<String> valores = new ArrayList<String>();
         //variaveis de controle (phase e round)
-        int phase = 0;
+        int phase = 0; //inicio de phase
         int round = 0;
+        int n = 5;
+        int valor_default = 0;
+        int majority = 0;
+        int mult = 0;
+        int process_id = -1;
+        int f = 1; //processo malicioso
         //verificar existencia de processos na rede
-        try {
-            s = new MulticastSocket(6789);
-            String[] saida = args[0].split("/");
-            InetAddress group = InetAddress.getByName(saida[1]);
-            s.joinGroup(group);
-            String nova_saida = saida[0]+"/"+saida[1]+"/"+saida[2];
-            byte[] data = nova_saida.getBytes();
-            DatagramPacket alfa = new DatagramPacket(data, data.length, group, 6789);
-            s.send(alfa);
-            byte[] buffer;
-            buffer = new byte[1000];
-            while (processos.size() < 5) {
-                DatagramPacket beta = new DatagramPacket(buffer, buffer.length,group,6789);
-                s.receive(beta);
-                String[] entrada = new String(beta.getData()).split("/");
-                boolean contains = processos.contains(entrada[2]);
-                //verifica se o processo recebido ja esta localizado no array
-                if(!contains){
-                    processos.add(entrada[2]);
-                    valores.add(entrada[0]);
-                    System.out.println("Received:" + String.valueOf(entrada[0])+"---"+String.valueOf(entrada[2]));
+        for (phase = 1; phase <= f + 1; phase++) {
+            try {
+                s = new MulticastSocket(6789);
+                String[] saida = args[0].split("/");
+                InetAddress group = InetAddress.getByName(saida[1]);
+                s.joinGroup(group);
+                String nova_saida = saida[0] + "/" + saida[1] + "/" + saida[2];
+                byte[] data = nova_saida.getBytes();
+                DatagramPacket alfa = new DatagramPacket(data, data.length, group, 6789);
+                s.send(alfa);
+                byte[] buffer;
+                buffer = new byte[1000];
+                while (processos.size() < 5) {
+                    DatagramPacket beta = new DatagramPacket(buffer, buffer.length, group, 6789);
+                    s.receive(beta);
+                    String[] entrada = new String(beta.getData()).split("/");
+                    boolean contains = processos.contains(entrada[2]);
+                    process_id = Integer.parseInt(entrada[2]);
+                    //verifica se o processo recebido ja esta localizado no array
+                    if (!contains) {
+                        processos.add(entrada[2]);
+                        valores.add(entrada[0]);
+                        System.out.println("Received:" + String.valueOf(entrada[0]) + "---" + String.valueOf(entrada[2]));
+                    }
                 }
+                System.out.println("PHASE 1");
+
+            } catch (SocketException e) {
+                System.out.println("Socket: " + e.getMessage());
+            } catch (IOException e) {
+                System.out.println("IO: " + e.getMessage());
+            } finally {
+                if (s != null) s.close();
             }
-        } catch (SocketException e) {
-            System.out.println("Socket: " + e.getMessage());
-        } catch (IOException e) {
-            System.out.println("IO: " + e.getMessage());
-        } finally {
-            if (s != null) s.close();
         }
     }
 
