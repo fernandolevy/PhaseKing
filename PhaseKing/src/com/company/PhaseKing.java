7bc8c6ca0bba1e6413e526dd7a753e8dad5d2abd
fernandolevy <fernandoassis@alunos.utfpr.edu.br>
2019-09-19 21:28:04 -0500

    finish v1

--- PhaseKing/src/com/company/PhaseKing.java
+++ PhaseKing/src/com/company/PhaseKing.java
@@ -1,8 +1,17 @@
 package com.company;
+//pega os dados do filme, converte para JSON e armazena em string
 
+import java.util.ArrayList;
 import java.io.IOException;
 import java.net.*;
 import java.util.Arrays;
+
+import com.google.gson.Gson;
+
+class Message {
+    public String id;
+    public String v;
+}
 
 public class PhaseKing {
     public static void main(String[] args) throws IOException {
@@ -19,32 +28,48 @@ public class PhaseKing {
 
         int n = 5;
         int f = 1;
-        int flagReceive = 0;
-
         DatagramPacket messageOut;
         DatagramPacket messageIn;
 
+        Message message_json = new Message();
+        Gson gson = new Gson();
         MulticastSocket s = null;
         InetAddress group = InetAddress.getByName(args[1]);
-        s = new MulticastSocket(6789);
-        s.joinGroup(group);
 
         for (int phase = 0; phase <= f; phase++) {
+            s = new MulticastSocket(6789);
+            s.joinGroup(group);
+            message_json.id = args[2];
+            message_json.v = v;
+            String message_string_out = gson.toJson(message_json);
+            m = message_string_out.getBytes();
 
-            // args give message contents and destination multicast group (e.g. "228.5.6.7")
-            m = v.getBytes();
             messageOut = new DatagramPacket(m, m.length, group, 6789);
             s.send(messageOut);
-            buffer = new byte[1000];
-            for (int j = 0; j < n; j++) {        // get messages from others in group
-                messageIn = new DatagramPacket(buffer, buffer.length);
-                s.receive(messageIn);
-                System.out.println("Received round 1:" + new String(messageIn.getData()));
-                if (Arrays.toString(messageIn.getData()).equals("1")) {
-                    one = one + 1;
-                } else {
-                    zero = zero + 1;
+
+            buffer = new byte[18];
+            while (n <= 5) {
+                s.setSoTimeout(10000);
+                for (int j = 0; j < n; j++) {
+                    try {
+                        // get messages from others in group
+                        messageIn = new DatagramPacket(buffer, buffer.length);
+                        s.receive(messageIn);
+                        Message message_string_in = gson.fromJson(new String(messageIn.getData()), Message.class);
+                        System.out.println("Received round 1 - " + "id:" + message_string_in.id + " v:" + message_string_in.v);
+
+                        if (message_string_in.v.equals("1")) {
+                            one = one + 1;
+                        } else {
+                            zero = zero + 1;
+                        }
+                    }catch (SocketTimeoutException e) {
+                        // timeout exception.
+                        System.out.println("Timeout reached!!! " + e);
+                        n = n - 1;
+                    }
                 }
+                break;
             }
             if (one > n / 2) {
                 majority = 1;
@@ -52,22 +77,39 @@ public class PhaseKing {
             } else if (zero > n / 2) {
                 majority = 0;
                 mult = zero;
+            } else {
+                majority = 1;
             }
 
-            System.out.println("round 2" + " " + String.valueOf(i) + " " + String.valueOf(phase));
             if (i == phase) {
-                m = String.valueOf(majority).getBytes();
+                message_json.id = args[2];
+                message_json.v = String.valueOf(majority);
+                message_string_out = gson.toJson(message_json);
+                m = message_string_out.getBytes();
+
                 messageOut = new DatagramPacket(m, m.length, group, 6789);
                 System.out.println("Received round 2 - Send Majority:" + new String(messageOut.getData()));
                 s.send(messageOut);
                 tiebreaker = v;
 
             } else {
-                messageIn = new DatagramPacket(buffer, buffer.length);
-                s.receive(messageIn);
-                s.setTimeToLive(10);
-                System.out.println("Received round 2 - Received Majority:" + new String(messageIn.getData()));
-                tiebreaker = new String(messageIn.getData());
+                s.setSoTimeout(1000);   // set the timeout in millisecounds.
+                while (true) {        // recieve data until timeout
+                    try {
+                        messageIn = new DatagramPacket(buffer, buffer.length);
+                        s.receive(messageIn);
+                        Message message_string_in = gson.fromJson(new String(messageIn.getData()), Message.class);
+
+                        System.out.println("Received round 2 - Received Majority:" + "id:" + message_string_in.id + " v:" + message_string_in.v);
+                        tiebreaker = message_string_in.v;
+                    } catch (SocketTimeoutException e) {
+                        // timeout exception.
+                        System.out.println("Timeout reached!!! " + e);
+                        s.close();
+                        tiebreaker = "1";
+                        break;
+                    }
+                }
             }
             if (mult > n / 2 + f) {
                 v = String.valueOf(majority);
@@ -75,10 +117,6 @@ public class PhaseKing {
                 v = tiebreaker;
             }
             if (phase == f) {
-                m = String.valueOf(v).getBytes();
-                messageOut = new DatagramPacket(m, m.length, group, 6789);
-                System.out.println("Received round 2 - Send Majority:" + new String(messageOut.getData()));
-                s.send(messageOut);
                 System.out.println("output decision value v:" + v);
             }
         }
